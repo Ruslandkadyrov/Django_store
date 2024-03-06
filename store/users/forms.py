73735d1d448791ca7573c79934fdf0444f547707
Кадыров Руслан <kadyrovrr88@mail.ru>
2024-03-07 02:24:02 +0500

    Реализована регистрация пользователя

--- store/users/forms.py
+++ store/users/forms.py
@@ -4,11 +4,20 @@ from django import forms
 
 
 class NewUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
     username = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
     password1 = forms.CharField(required=True)
     password2 = forms.CharField(required=True)
 
     class Meta:
         model = User
-        fields = ('email', 'username', 'password1', 'passowrd2')
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'username',
+            'password1',
+            'password2'
+        )
