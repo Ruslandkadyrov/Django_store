cc7930dd8c4ac88a8db28989ed73c35faa7874e1
Кадыров Руслан <kadyrovrr88@mail.ru>
2024-05-11 19:58:12 +0500

    Реализовано Оформление заказа

--- store/users/forms.py
+++ store/users/forms.py
@@ -4,22 +4,14 @@ from django import forms
 
 
 class NewUserForm(UserCreationForm):
-    first_name = forms.CharField(required=True)
-    last_name = forms.CharField(required=True)
-    username = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
-    contact_number = forms.CharField(required=True)
-    town = forms.CharField(required=True)
-    adress = forms.CharField(required=True)
-    password1 = forms.CharField(required=True)
-    password2 = forms.CharField(required=True)
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirmation = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = CustomUser
         fields = (
             'first_name',
             'last_name',
-            'email',
             'username',
             'contact_number',
             'town',
